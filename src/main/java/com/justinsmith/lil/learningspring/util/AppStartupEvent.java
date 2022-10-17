9d17f52ee9082f14b0ae19265dbfef8fffb779c3
jsmith3763 <jsmith3763@gmail.com>
2022-10-17 14:29:10 -0700

    Set up embedded DB so far

---
+++ src/main/java/com/justinsmith/lil/learningspring/util/AppStartupEvent.java
@@ -0,0 +1,29 @@
+package com.justinsmith.lil.learningspring.util;
+
+import com.justinsmith.lil.learningspring.data.*;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.reservationRepository = reservationRepository;
+        this.guestRepository = guestRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        Iterable<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
+    }
+}
